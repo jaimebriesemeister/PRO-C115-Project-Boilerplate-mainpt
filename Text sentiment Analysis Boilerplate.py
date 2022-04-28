f271a93a3958ea057c4b859cf38aefd731b3ac2e
jaimebriesemeister <82653221+jaimebriesemeister@users.noreply.github.com>
2022-04-28 19:23:45 -0300

    Add files via upload

---
+++ Text sentiment Analysis Boilerplate.py
@@ -0,0 +1,18 @@
+import tensorflow.keras
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+sentence = ["Câmera incrível. Vale o preço"]
+
+# Tokenização
+
+# Crie um dicionário chamado word_index
+
+# Preenchendo a sequência
+
+# Defina o modelo usando um arquivo .h5
+
+# Teste o modelo
+
+# Imprima o resultado
